fe26941a3d04b74ba3a38312477544db391921fb
HongNanfang <hongnanfang@HongNanfangdeMacBook.local>
2018-02-18 14:57:12 -0500

    Professor Haviland's comment: perfect!

--- Apprehension_Shiny/server.R
+++ Apprehension_Shiny/server.R
@@ -1,30 +1,20 @@
 function(input, output) {
   
-  filedata <- reactive({
-    infile <- input$datafile
-    if (is.null(infile)) {
-      # User has not uploaded a file yet
-      return(NULL)
-    }
-    read.csv(infile$datapath)
-  })
+  filedata <- read.csv('https://s3.us-east-2.amazonaws.com/nanfang/PB+monthly+summaries.csv')
   
-  output$main_plot <- renderPlot({
+    output$main_plot <- renderPlot({
+      ts2 <- filedata
+      ts4 <- ts2[2:13]
+      ts5 <- rev(as.vector(t(rev(ts4))))
+      n <- length(ts5)
+      ts6 <- sapply(1:(n - input$n_month + 1), function(i) mean(ts5[i:(i+input$n_month-1)]))
+      ts5 <- ts(ts6, start = c(2000,10), frequency=12)
+      ts.plot(ts5, gpars=list(xlab="year", ylab="Apprehensions", lty=c(1:3)), main = "Historical Apprehension data from 2000 to 2017")
+    })
+  
+  
+  
 
-    if (is.null(filedata()))
-      return(NULL)
-    
-    ts2 <- filedata()
-    ts4 <- ts2[2:13]
-    ts5 <- rev(as.vector(t(rev(ts4))))
-    n <- length(ts5)
-    ts6 <- sapply(1:(n - input$n_month + 1), function(i) mean(ts5[i:(i+input$n_month-1)]))
-    ts5 <- ts(ts6, start = c(2000,10), frequency=12)
-    ts.plot(ts5, gpars=list(xlab="year", ylab="Apprehensions", lty=c(1:3)), main = "Historical Apprehension data from 2000 to 2017")
-  
-    
-    
-  })
   
   
 }
--- Apprehension_Shiny/ui.R
+++ Apprehension_Shiny/ui.R
@@ -1,26 +1,27 @@
-
+df2010 <- read.csv("https://s3.us-east-2.amazonaws.com/nanfang/BP+Apprehensions+2010.csv")
+dMat2010 <- gsub(",", "", as.matrix(df2010[1:9,2:13]))
+storage.mode(dMat2010) <- "numeric"
+df2017 <- read.csv("https://s3.us-east-2.amazonaws.com/nanfang/PB+Apprehensions+2017.csv")
+dMat2017 <- gsub(",", "", as.matrix(df2017[1:9,2:13]))
+storage.mode(dMat2017) <- "numeric"
 
 
 fluidPage(
   titlePanel("US Border Apprehensions Showroom"),
   sidebarLayout(
     sidebarPanel(
-      fileInput('datafile', 'PB monthly summaries.csv',
-                accept = c(
-                  'text/csv',
-                  'text/comma-separated-values',
-                  'text/tab-separated-values',
-                  'text/plain',
-                  '.csv',
-                  '.tsv'
-                )
-      ),
       sliderInput(inputId = "n_month",
                   label = "Bandwidth adjustment:",
                   min = 1, max = 72, value = 1, step = 1)
+      #selectInput("year", "Year:", 
+      #            choices=c(2010, 2017)),
+      #selectInput("sector", "Sector:", 
+      #            choices= df2010$Sector)
     ),
-    
-      plotOutput("main_plot", height = "500px")
-    
+    plotOutput("main_plot")
   )
+  
+  
+  
+  
 )
