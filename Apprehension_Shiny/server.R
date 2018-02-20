e5f53f7ead74aede34a209252336415f74f756a1
HongNanfang <hongnanfang@dhcp-wifi-8021x-155-41-29-167.bu.edu>
2018-02-19 19:10:07 -0500

    Add another graph. Now finished. https://nanfang.shinyapps.io/Apprehension_Shiny/

--- Apprehension_Shiny/server.R
+++ Apprehension_Shiny/server.R
@@ -1,8 +1,18 @@
 function(input, output) {
-  
+  df2010 <- read.csv("https://s3.us-east-2.amazonaws.com/nanfang/BP+Apprehensions+2010.csv")
+  dMat2010 <- gsub(",", "", as.matrix(df2010[1:9,2:13]))
+  storage.mode(dMat2010) <- "numeric"
+  rownames(dMat2010) <- df2010$Sector
+  df2017 <- read.csv("https://s3.us-east-2.amazonaws.com/nanfang/PB+Apprehensions+2017.csv")
+  dMat2017 <- gsub(",", "", as.matrix(df2017[1:9,2:13]))
+  storage.mode(dMat2017) <- "numeric"
+  rownames(dMat2017) <- df2010$Sector
   filedata <- read.csv('https://s3.us-east-2.amazonaws.com/nanfang/PB+monthly+summaries.csv')
   
-    output$main_plot <- renderPlot({
+  
+  output$main_plot <- renderPlot({
+    
+    if (input$graphSelection == 'Historical Time Series'){
       ts2 <- filedata
       ts4 <- ts2[2:13]
       ts5 <- rev(as.vector(t(rev(ts4))))
@@ -10,7 +20,23 @@ function(input, output) {
       ts6 <- sapply(1:(n - input$n_month + 1), function(i) mean(ts5[i:(i+input$n_month-1)]))
       ts5 <- ts(ts6, start = c(2000,10), frequency=12)
       ts.plot(ts5, gpars=list(xlab="year", ylab="Apprehensions", lty=c(1:3)), main = "Historical Apprehension data from 2000 to 2017")
-    })
+    }
+    if (input$graphSelection == '2010 vs. 2017 Data'){
+      if (input$year == 2010){
+        barplot(dMat2010[(input$sector), ], main = paste('2010',input$sector, 'Monthly Apprehensions Data'))
+      }
+      if (input$year == 2017){
+        barplot(dMat2017[(input$sector), ], main = paste('2017',input$sector, 'Monthly Apprehensions Data'))
+      }
+      
+      
+      
+    }
+    
+    
+  })
+  
+    
   
   
   
--- Apprehension_Shiny/ui.R
+++ Apprehension_Shiny/ui.R
@@ -1,25 +1,32 @@
 df2010 <- read.csv("https://s3.us-east-2.amazonaws.com/nanfang/BP+Apprehensions+2010.csv")
-dMat2010 <- gsub(",", "", as.matrix(df2010[1:9,2:13]))
-storage.mode(dMat2010) <- "numeric"
-df2017 <- read.csv("https://s3.us-east-2.amazonaws.com/nanfang/PB+Apprehensions+2017.csv")
-dMat2017 <- gsub(",", "", as.matrix(df2017[1:9,2:13]))
-storage.mode(dMat2017) <- "numeric"
 
-
-fluidPage(
-  titlePanel("US Border Apprehensions Showroom"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "n_month",
-                  label = "Bandwidth adjustment:",
-                  min = 1, max = 72, value = 1, step = 1)
-      #selectInput("year", "Year:", 
-      #            choices=c(2010, 2017)),
-      #selectInput("sector", "Sector:", 
-      #            choices= df2010$Sector)
-    ),
-    plotOutput("main_plot")
-  )
+bootstrapPage(
+  
+  selectInput(inputId = "graphSelection",
+              label = "Choose a graph:",
+              choices = c('Historical Time Series', '2010 vs. 2017 Data'),
+              selected = 'Historical Time Series'),
+  # Display this only if the input.graphSelection == 'Historical Time Series'
+  conditionalPanel(condition = "input.graphSelection == 'Historical Time Series'",
+                   sliderInput(inputId = "n_month",
+                               label = "Bandwidth adjustment:",
+                               min = 1, max = 72, value = 1, step = 1)
+  ),
+  # Display this only if the input.graphSelection == '2010 vs. 2017 Data'
+  conditionalPanel(condition = "input.graphSelection == '2010 vs. 2017 Data'",
+                   selectInput("year", "Year:", choices=c(2010, 2017)),
+                   selectInput("sector", "Sector:", choices= c('Big Bend', 
+                                                               'Del Rio',
+                                                               'El Centro',
+                                                               'El Paso',
+                                                               'Laredo',
+                                                               'Rio Grande Valley',
+                                                               'San Diego',
+                                                               'Tucson',
+                                                               'Yuma'))
+  ),
+  
+  plotOutput(outputId = "main_plot", height = "400px")
   
   
   
